a4d5f7924d4f836d3238747971d746703874aed2
koide-g <koide-g@acaric.co.jp>
2018-08-10 12:33:21 +0900

    use env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
   before_action :multiple_authenticate
-  
   
   protected
 
   def multiple_authenticate
+    # ENV['BASIC_AUTH_PAIRS']
+    # => user1:password1,user2:password2,user3:password3
+    return true if ENV['BASIC_AUTH_PAIRS'].nil?
     authenticate_or_request_with_http_basic do |username, password|
-      (
-        ActiveSupport::SecurityUtils.secure_compare(username, 'user1') &
-        ActiveSupport::SecurityUtils.secure_compare(password, 'pass1')
-      ) | (
-        ActiveSupport::SecurityUtils.secure_compare(username, 'user2') &
-        ActiveSupport::SecurityUtils.secure_compare(password, 'pass2')
-      )
+      
+      ENV['BASIC_AUTH_PAIRS'].split(',')
+        .map { |x| x.split(':') }
+        .map { |user, passwd|
+         ActiveSupport::SecurityUtils.secure_compare(username, user) &
+         ActiveSupport::SecurityUtils.secure_compare(password, passwd)
+        }
+        .any?
     end
   end
 end
