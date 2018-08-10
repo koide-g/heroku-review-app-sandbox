51ecacd964e8081d33e28579471e79fcf2c5499c
koide-g <koide-g@acaric.co.jp>
2018-08-10 11:45:23 +0900

    add multiple basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  before_action :multiple_authenticate
+  
+  
+  protected
+
+  def multiple_authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      (
+        ActiveSupport::SecurityUtils.secure_compare(username, 'user1') &
+        ActiveSupport::SecurityUtils.secure_compare(password, 'pass1')
+      ) | (
+        ActiveSupport::SecurityUtils.secure_compare(username, 'user2') &
+        ActiveSupport::SecurityUtils.secure_compare(password, 'pass2')
+      )
+    end
+  end
 end
